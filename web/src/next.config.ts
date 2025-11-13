45c299f2714db1a32167dc987f74d989e993459a
Hao Yan <haoyan160@gmail.com>
2025-11-12 23:36:41 -0500

    Update dependencies

--- web/src/next.config.ts
+++ web/src/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const path = require("path");
+
 const nextConfig: NextConfig = {
     output: "export",
     distDir: "../out",
+    outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
