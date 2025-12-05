1bb5afc6380c91a3a94d3ded89f32e0347f82756
Hao Yan <haoyan160@gmail.com>
2025-12-05 03:13:52 -0500

    Implement early stopping to determine effect of overfitting
    Model prediction error for individual users decreased to 10.25% (previously 13.13%)

--- server/pipelines/train.py
+++ server/pipelines/train.py
@@ -9,36 +9,51 @@ from src.components.trainer import Trainer
 from src.components.model import IntervalPredictor
 
 def split_data(x, y, train = 0.8, val = 0.1):
-    x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size = 1 - train)
+    x_train, x_temp, y_train, y_temp = train_test_split(
+        x, 
+        y,
+        test_size = 1 - train, 
+        random_state = 42
+    )
+    
     val_ratio = val / (1 - train)
-    x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size = 1 - val_ratio)
+    
+    x_val, x_test, y_val, y_test = train_test_split(
+        x_temp, 
+        y_temp, 
+        test_size = 1 - val_ratio, 
+        random_state = 42
+    )
     
     return x_train, x_val, x_test, y_train, y_val, y_test
 
 if __name__ == "__main__":
     DATA_PATH = Path("../data/seed")
+    MODEL_SAVE_PATH = Path("../models/FNN_model.pth")
 
     x = np.load(DATA_PATH / "x.npy")
     y = np.load(DATA_PATH / "y.npy")
-
-    scaler = StandardScaler()
-    x_scaled = scaler.fit_transform(x)
     
-    x_train, x_val, x_test, y_train, y_val, y_test = split_data(x_scaled, y)
+    x_train, x_val, x_test, y_train, y_val, y_test = split_data(x, y)
+    
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_val_scaled = scaler.fit_transform(x_val)
+    x_test_scaled = scaler.fit_transform(x_test)
     
     train_ds = TensorDataset(
-        torch.tensor(x_train, dtype = torch.float32),
+        torch.tensor(x_train_scaled, dtype = torch.float32),
         torch.tensor(y_train, dtype = torch.float32).unsqueeze(-1)
     )
     
     val_ds = TensorDataset(
-        torch.tensor(x_train, dtype = torch.float32),
-        torch.tensor(y_train, dtype = torch.float32).unsqueeze(-1)
+        torch.tensor(x_val_scaled, dtype = torch.float32),
+        torch.tensor(y_val, dtype = torch.float32).unsqueeze(-1)
     )
     
     test_ds = TensorDataset(
-        torch.tensor(x_train, dtype = torch.float32),
-        torch.tensor(y_train, dtype = torch.float32).unsqueeze(-1)
+        torch.tensor(x_test_scaled, dtype = torch.float32),
+        torch.tensor(y_test, dtype = torch.float32).unsqueeze(-1)
     )
     
     train_loader = DataLoader(train_ds, batch_size = 32, shuffle = True)
@@ -46,17 +61,14 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_ds, batch_size = 32)
 
     model = IntervalPredictor(input_size = x.shape[1])
-    
     trainer = Trainer(model)
-
-    for epoch in range(150):
-        train_loss = trainer.train(train_loader)
-        val_loss = trainer.validate(val_loader)
-
-        if not epoch % 20:
-            print(f"Epoch {epoch}, Train Loss = {train_loss:.4f}, Val Loss = {val_loss:.4f}")
-
-    torch.save(model.state_dict(), "../models/FNN_model.pth")
+    
+    trainer.fit(
+        train_loader = train_loader,
+        val_loader = val_loader,
+        epochs = 150,
+        checkpoint_path = MODEL_SAVE_PATH
+    )
 
     np.save(DATA_PATH / "scaler_mean.npy", scaler.mean_)
     np.save(DATA_PATH / "scaler_scale.npy", scaler.scale_)
--- server/src/components/trainer.py
+++ server/src/components/trainer.py
@@ -3,12 +3,16 @@ import torch.nn as nn
 import torch.optim as optim
 
 class Trainer:
-    def __init__(self, model, lr = 0.001, device = None):
+    def __init__(self, model: nn.Module, lr = 0.001, scheduler = None, device = None):
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
         self.model = model.to(self.device)
-        
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(model.parameters(), lr = lr)
+        self.scheduler = scheduler
+        self.history = {
+            'train_loss': [], 
+            'val_loss': []
+        }
         
     def train(self, loader):
         self.model.train()
@@ -47,3 +51,37 @@ class Trainer:
                 total_loss += loss.item()
                 
         return total_loss / len(loader)
+    
+    def fit(self, train_loader, val_loader, epochs: int, checkpoint_path = None):
+        best_val_loss = float("inf")
+        
+        for epoch in range(epochs):
+            train_loss = self.train(train_loader)
+            val_loss = self.validate(val_loader)
+            
+            self.history['train_loss'].append(train_loss)
+            self.history['val_loss'].append(val_loss)
+            
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                epochs_no_improve = 0 
+                
+                torch.save(self.model.state_dict(), checkpoint_path)
+                
+                print(f"Epoch {epoch:3d}/{epochs}: Val Loss: {val_loss:.4f} (Saved best model)")
+            else:
+                epochs_no_improve += 1
+                
+                print(f"Epoch {epoch:3d}/{epochs}: Val Loss: {val_loss:.4f} (No improvement)")
+                
+            if epochs_no_improve >= 15:
+                print(f"Early stop")
+                
+                self.model.load_state_dict(torch.load(checkpoint_path))
+                
+                break
+                
+            if self.scheduler is not None and isinstance(self.scheduler, optim.lr_scheduler.ReduceLROnPlateau):
+                self.scheduler.step(val_loss)
+                
+        print(f"Training finished. Best Val Loss: {best_val_loss:.4f}")
